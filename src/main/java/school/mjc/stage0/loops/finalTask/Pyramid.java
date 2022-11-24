191b9bb4ef6390f950decc215c5060c7514fc8bc
Sano <sanasar.hambardzumyan@gmail.com>
2022-11-24 22:48:59 +0400

    Changes

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,21 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-        int rowCount = 1;
-        for (int i = cathetusLength; i > 0; i--) {
-            for (int j = 1; j <= i*2; j++) {
+
+        for (int i = 1; i <= cathetusLength; i++) {
+
+            for (int j = 1; j <= cathetusLength - i; j++) {
                 System.out.print(" ");
             }
 
-            for (int j = 1; j <= rowCount; j++) {
-                System.out.print(j+" ");
+            for (int j = i; j >= 1 ; j--) {
+                System.out.print(j);
             }
-
-            for (int j = rowCount-1; j >= 1; j--) {
-                System.out.print(j+" ");
+            for (int j = 2; j <= i ; j++) {
+                System.out.print(j);
             }
             System.out.println();
-            rowCount++;
         }
     }
 
