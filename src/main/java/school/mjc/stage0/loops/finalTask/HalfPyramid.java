82fe0f7bbe3320d15ce8b5b679f12d489713089e
Sano <sanasar.hambardzumyan@gmail.com>
2022-11-24 22:31:37 +0400

    Changed

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -3,6 +3,25 @@ package school.mjc.stage0.loops.finalTask;
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
 
+        int k = 1;
+        for (int i = cathetusLength; i > 0 ; i--) {
+            printSpace(i - 1);
+            printAst(k);
+            k++;
+            System.out.println();
+        }
+    }
+
+    public void printAst(int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+    }
+
+    public void printSpace(int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print(" ");
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int rowCount = 1;
+        for (int i = cathetusLength; i > 0; i--) {
+            for (int j = 1; j <= i*2; j++) {
+                System.out.print(" ");
+            }
 
+            for (int j = 1; j <= rowCount; j++) {
+                System.out.print(j+" ");
+            }
+
+            for (int j = rowCount-1; j >= 1; j--) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+            rowCount++;
+        }
     }
 
     public static void main(String[] args) {
